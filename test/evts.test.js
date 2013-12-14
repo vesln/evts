184ed0946d7bf23e0ee0e7569b7e8bf683c93f1a
George Stagas <gstagas@gmail.com>
2013-12-14 12:38:01 -0800

    add `.off()`

--- test/evts.test.js
+++ test/evts.test.js
@@ -152,14 +152,43 @@ describe('evts', function() {
     evts.on('test', function() {
       actual.push(1);
     });
+
     evts.once('test', function() {
       actual.push(2);
     });
+
     evts.on('test', function() {
       actual.push(3);
     });
 
     evts.emit('test', function() {
+      evts.emit('test', function() {
+        actual.should.eql(expected);
+      });
+    });
+  });
+
+  it('can remove event listeners', function() {
+    var evts = new EventEmitter;
+    var actual = [];
+    var expected = [1, 2, 3, 1, 3];
+
+    evts.on('test', function() {
+      actual.push(1);
+    });
+
+    evts.on('test', second);
+
+    evts.on('test', function() {
+      actual.push(3);
+    });
+
+    function second() {
+      actual.push(2);
+    }
+
+    evts.emit('test', function() {
+      evts.off('test', second);
       evts.emit('test', function() {
         actual.should.eql(expected);
       });
